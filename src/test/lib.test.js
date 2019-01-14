bfa1c1d249e674cdd9e2b00f24bbd8df4d54211b
student <student@localhost>
2019-01-14 23:12:14 +0300

    lib.js & lib.test.js edited

--- src/test/lib.test.js
+++ src/test/lib.test.js
@@ -1,11 +1,27 @@
 import {cofemat} from "../js/lib";
 
 test('select number of drink, put money and get change', () => {
+    const drinkNumb = 10;
+    const money = 100;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(100);
+});
+
+test('select number of drink, put money and get change', () => {
     const drinkNumb = 40;
     const money = 100;
     const result = cofemat(drinkNumb, money);
 
     expect(result).toBe(60);
+});
+
+test('select number of drink, put money and get change', () => {
+    const drinkNumb = 40;
+    const money = 20;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(20);
 });
 
 test('select number of drink, put money and get change', () => {
@@ -17,9 +33,41 @@ test('select number of drink, put money and get change', () => {
 });
 
 test('select number of drink, put money and get change', () => {
+    const drinkNumb = 45;
+    const money = 20;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(20);
+});
+
+test('select number of drink, put money and get change', () => {
     const drinkNumb = 30;
     const money = 25;
     const result = cofemat(drinkNumb, money);
 
     expect(result).toBe(25);
+});
+
+test('select number of drink, put money and get change', () => {
+    const drinkNumb = 30;
+    const money = 100;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(70);
+});
+
+test('select number of drink, put money and get change', () => {
+    const drinkNumb = 35;
+    const money = 100;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(65);
+});
+
+test('select number of drink, put money and get change', () => {
+    const drinkNumb = 35;
+    const money = 20;
+    const result = cofemat(drinkNumb, money);
+
+    expect(result).toBe(20);
 });
