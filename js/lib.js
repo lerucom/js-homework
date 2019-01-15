d12a8371c23784cf51a7d62ce0d1bdff026413a7
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-15 14:12:55 +0000

    Deploy lerucom/js-homework1 to github.com/lerucom/js-homework1.git:gh-pages

--- js/lib.js
+++ js/lib.js
@@ -1,49 +1,42 @@
 export function cofemat(drinkNumb, money) {
     let change = money; // если выбрал несуществующий номер напитка, возвращается вся сумма
 
-        switch (drinkNumb) {
-            case 30: {
-                let coffee3In1 = 30;
-                if (money >= coffee3In1) {
-                    change = money - coffee3In1;
-                } else {
-                    change = money; //если денег не хватает, возвращаем всю внесенную сумму
-                }
+    switch (drinkNumb) {
+        case 30: {
+            let coffee3In1 = 30;
+            if (money >= coffee3In1) {
+                change = money - coffee3In1;
             }
-                break;
-
-            case 35: {
-                let coffeeBlack = 35;
-                if (money >= coffeeBlack) {
-                    change = money - coffeeBlack;
-                } else {
-                    change = money; //если денег не хватает, возвращаем всю внесенную сумму
-                }
-            }
-                break;
-
-            case 40: {
-                let coffeeCream = 40;
-                if (money >= coffeeCream) {
-                    change = money - coffeeCream;
-                } else {
-                    change = money; //если денег не хватает, возвращаем всю внесенную сумму
-                }
-            }
-                break;
-
-            case 45: {
-                let coffeeLatte = 45;
-                if (money >= coffeeLatte) {
-                    change = money - coffeeLatte;
-                } else {
-                    change = money; //если денег не хватает, возвращаем всю внесенную сумму
-                }
-            }
-                break;
-
-            default: change = money;
-
         }
-        return change;
+            break;
+
+        case 35: {
+            let coffeeBlack = 35;
+            if (money >= coffeeBlack) {
+                change = money - coffeeBlack;
+            }
+        }
+            break;
+
+        case 40: {
+            let coffeeCream = 40;
+            if (money >= coffeeCream) {
+                change = money - coffeeCream;
+            }
+        }
+            break;
+
+        case 45: {
+            let coffeeLatte = 45;
+            if (money >= coffeeLatte) {
+                change = money - coffeeLatte;
+            }
+        }
+            break;
+
+        default:
+            change = money;
+    }
+
+    return change;
 }
