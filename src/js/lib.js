f502ac531279eae0fbefd38233c9caa5de4b809a
student <student@localhost>
2019-01-17 10:14:14 +0300

    Fixed #3, Removed block duplication in lib.js.

--- src/js/lib.js
+++ src/js/lib.js
@@ -1,39 +1,14 @@
 export function cofemat(drinkNumb, money) {
     let change = money; // если выбрал несуществующий номер напитка, возвращается вся сумма
+    const coffees = { // можно было сделать через массив объектов, чтобы хранить доп. инфу о напитке
+        30: 30,
+        35: 35,
+        40: 40,
+        45: 45
+    };
 
-    switch (drinkNumb) {
-        case 30: {
-            let coffee3In1 = 30;
-            if (money >= coffee3In1) {
-                change = money - coffee3In1;
-            }
-        }
-            break;
-
-        case 35: {
-            let coffeeBlack = 35;
-            if (money >= coffeeBlack) {
-                change = money - coffeeBlack;
-            }
-        }
-            break;
-
-        case 40: {
-            let coffeeCream = 40;
-            if (money >= coffeeCream) {
-                change = money - coffeeCream;
-            }
-        }
-            break;
-
-        case 45: {
-            let coffeeLatte = 45;
-            if (money >= coffeeLatte) {
-                change = money - coffeeLatte;
-            }
-        }
-            break;
+    if (money >= drinkNumb && coffees[drinkNumb]) {
+        change = money - drinkNumb;
     }
-
     return change;
 }
