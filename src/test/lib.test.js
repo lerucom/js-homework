4863f68a6b163cd997ec661e69db7e6ef1f39f46
lerucom <lerucom@gmail.com>
2019-01-24 16:50:05 +0300

    tests fixed

--- src/test/lib.test.js
+++ src/test/lib.test.js
@@ -1,73 +1,18 @@
 import {cofemat} from "../js/lib";
 
-test('select unknown drinkNumb = 10 and put money = 100, then get change = 100', () => {
-    const drinkNumb = 10;
-    const money = 100;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(100);
-});
-
-test('select drinkNumb = 40, and put money = 100, then get change = 60', () => {
-    const drinkNumb = 40;
-    const money = 100;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(60);
-});
-
-test('select drinkNumb = 40, and put money = 20, then get change = 20', () => {
-    const drinkNumb = 40;
-    const money = 20;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(20);
-});
-
-test('select drinkNumb = 45, and put money = 100, then get change = 55', () => {
-    const drinkNumb = 45;
-    const money = 100;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(55);
-});
-
-test('select drinkNumb = 45, and put money = 20, then get change = 20', () => {
-    const drinkNumb = 45;
-    const money = 20;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(20);
-});
-
-test('select drinkNumb = 30, and put money = 25, then get change = 25', () => {
-    const drinkNumb = 30;
-    const money = 25;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(25);
-});
-
-test('select drinkNumb = 30, and put money = 100, then get change = 70', () => {
-    const drinkNumb = 30;
-    const money = 100;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(70);
-});
-
-test('select drinkNumb = 35, and put money = 100, then get change = 65', () => {
-    const drinkNumb = 35;
-    const money = 100;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(65);
-});
-
-test('select drinkNumb = 35, and put money = 20, then get change = 20', () => {
-    const drinkNumb = 35;
-    const money = 20;
-    const result = cofemat(drinkNumb, money);
-
-    expect(result).toBe(20);
-});
+test.each([
+    ['americana', 10, 10],
+    ['americana', 100, 70],
+    ['cappuccino', 10, 10],
+    ['cappuccino', 100, 65],
+    ['espresso', 10, 10],
+    ['espresso', 100, 60],
+    ['latte', 10, 10],
+    ['latte', 100, 55]
+])(
+    'should actual drinkNumb %s, money %i, change is %i',
+    (drinkNumb, money, expected) => {
+        const actual = cofemat(drinkNumb, money);
+        expect(actual).toBe(expected);
+    }
+);
